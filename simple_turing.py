c0154b594bf9a2916c9fd4a3fb6493d343dbdaa9
Robby Stahl <beefnog@gmail.com>
2025-02-24 16:48:46 -0800

    Code cleanup, appeasing pylint...

--- simple_turing.py
+++ simple_turing.py
@@ -16,16 +16,19 @@ class TuringMachine:
         self.state = 'A'
         self.steps = 0
 
-        self.tape = dict() # {<pos_num>: 'contents'}
+        self.tape = {} # {<pos_num>: 'contents'}
         self.head_write('0')
         print("Machine created.")
-    
+
+
     def head_read(self):
         return self.tape[self.pos]
-    
+
+
     def head_write(self, value):
         self.tape[self.pos] = value
-    
+
+
     def head_left(self):
         self.pos -= 1
 
@@ -34,9 +37,8 @@ class TuringMachine:
             self.tape[self.pos]
         except KeyError:
             self.tape[self.pos] = 0
-        
-        return
-    
+
+
     def head_right(self):
         self.pos += 1
 
@@ -45,13 +47,11 @@ class TuringMachine:
             self.tape[self.pos]
         except KeyError:
             self.tape[self.pos] = 0
-        
-        return
-    
+
 
     def execute(self, deck):
-        self.contents = self.head_read()
-        combined = self.state + str(self.contents) # e.g. A0
+        contents = self.head_read()
+        combined = self.state + str(contents) # e.g. A0
         card_value = deck[combined]
 
         # 'A0' -> '1RB'
@@ -71,12 +71,10 @@ class TuringMachine:
         if 'L' == card_value[1]:
             print("moving left")
             self.head_left()
-        
+
         self.state = card_value[2]
 
         self.steps += 1
-        return
-
 
 
 
@@ -100,29 +98,30 @@ def get_deck():
     return predefined_deck
 
 
+
 def main():
-    
+
     deck = get_deck() # for now we have a hard-code deck returned from the function
-    if not type(deck) is dict:
+    if type(deck) is not dict:
         print("get_deck() failed to parse the provided deck. Halting...")
         print(type(deck))
         return
 
     tm = TuringMachine()
-    print(tm)
-    print("Step:", tm.steps, "State:", tm.state, "Pos:", tm.pos)
+    print("INIT\nStep:", tm.steps, "State:", tm.state, "Pos:", tm.pos, "\n")
     while tm.state != 'H':
         tm.execute(deck)
         print("Step:", tm.steps, "State:", tm.state, "Pos:", tm.pos)
         for k in sorted(tm.tape):
             print(k, tm.tape[k])
-        # input("\npush for go")
-    
+        print()
+
     print("\n\n\nHALT state reached.")
     print("Final tape:")
     for k in sorted(tm.tape):
         print(k, tm.tape[k])
     print("")
+
 
 
 if __name__ == "__main__":
